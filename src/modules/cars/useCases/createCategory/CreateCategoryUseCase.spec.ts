d089b1d5c69a40a7faca12328748034aa47b7a81
LuccaDi <lucca_di@hotmail.com>
2022-06-09 22:38:16 -0300

    installed jest

---
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -0,0 +1,15 @@
+describe("Create Category", () => {
+  it("The sum 2 + 2 should be equal 4", () => {
+    const sum = 2 + 2;
+    const result = 4;
+
+    expect(sum).toBe(result);
+  });
+
+  it("The sum 2 + 2 should not be equal 5", () => {
+    const sum = 2 + 2;
+    const result = 5;
+
+    expect(sum).not.toBe(result);
+  });
+});
